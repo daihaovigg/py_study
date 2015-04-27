a469904ed448c62315dfa1674e451bce430d9b9d
daihaovigg <865058222@qq.com>
2015-04-27 12:46:36 +0800

    process/threading/TCP/UDP

---
+++ service.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+
+
+# 导入socket库:
+import socket,time,threading
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind(('127.0.0.1',9999))   #this ip can be changed to your own ip
+
+s.listen(5)
+print 'Waiting for connection...'
+
+
+
+def tcplink(sock, addr):
+    print 'Accept new connection from %s:%s...' % addr
+    sock.send('Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if data == 'exit' or not data:
+            break
+        sock.send('Hello, %s!' % data)
+    sock.close()
+    print 'Connection from %s:%s closed.' % addr
+
+
+while True:
+# 接受一个新连接:
+  sock, addr = s.accept()
+# 创建新线程来处理TCP连接:
+  t = threading.Thread(target=tcplink, args=(sock, addr))
+  t.start()
+
+
---
+++ udpclient.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+
+
+# 导入socket库:
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+for data in ['Michael', 'Tracy', 'Sarah']:
+# 发送数据:
+  s.sendto(data, ('127.0.0.1', 9999))
+# 接收数据:
+  print s.recv(1024)
+s.close()
